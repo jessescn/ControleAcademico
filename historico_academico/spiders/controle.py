7917ba96ac132aae2fc71cb86b04d4f08550fdea
Jessé Souza <jesse.neto@ccc.ufcg.edu.br>
2019-09-14 21:41:44 -0300

    Adição de docstrings e pequenas alterações no codigo

--- historico_academico/spiders/controle.py
+++ historico_academico/spiders/controle.py
@@ -1,26 +1,43 @@
-# -*- coding: utf-8 -*-
-import scrapy
+"""Arquivo responsável por conter o Spider
+    referente ao controle acadêmico e suas funções auxiliares
+"""
 from getpass import getpass
 import os
+import scrapy
 
 def authentation_failed(response):
+    """ Método para checar se a autenticação falhou """
     return response.css('div.alert p::text').get() == 'Erro'
 
 class ControleSpider(scrapy.Spider):
+    """Spider do Controle Acadêmico
+
+    Spider responsável por acessar o controle
+    a partir da matricula e senha fornecidos pelo
+    usuário, para obter os dados das disciplinas
+    pagas pelo mesmo presentes no histórico.
+    """
     name = 'controle'
     start_urls = ['https://pre.ufcg.edu.br:8443/ControleAcademicoOnline/Controlador?command=Home']
 
     def parse(self, response):
+        """ Callback default da requisição as URLs presentes no array 'start_urls """
         print('\n--------------- AUTENTICAÇÃO ---------------\n')
-        matricula = input('Insira sua matrícula: ')
+        matricula = str(input('Insira sua matrícula: '))
         senha = getpass('Digite sua senha: ')
         return scrapy.FormRequest.from_response(
             response,
-            formdata={'login': str(matricula), 'senha':senha},
+            formdata={'login': matricula, 'senha':senha},
             callback=self.after_login
         )
 
     def after_login(self, response):
+        """ Callback da página de login
+
+        O método recebe a response e chama
+        o authentication_falied para checar se
+        os dados do login eram válidos.
+         """
         if not authentation_failed(response):
             yield scrapy.Request(response.urljoin('?command=AlunoHistorico'), callback=self.get_subjects)
         else:
@@ -28,25 +45,32 @@ class ControleSpider(scrapy.Spider):
             os.remove('historico.csv')
 
     def get_subjects(self, response):
-            table = response.xpath('//table[@class="table table-bordered"]/tbody/tr')
-            for tr in table:
-                data  = tr.xpath('./td//text()').getall()
-                sub_register = data[0]
-                sub_name = data[1]
-                sub_type = data[2]
-                sub_credits = data[3]
-                sub_workload = data[4]
-                sub_mean = data[5].strip().split('/n')[0]
-                sub_status = data[6]
-                sub_period = data[7]
+        """ Callback da página do histórico
 
-                yield  {
-                    "Código": sub_register,
-                    "Disciplina": sub_name,
-                    "Tipo": sub_type,
-                    "Créditos": sub_credits,
-                    "Carga horária": sub_workload,
-                    "Média": sub_mean,
-                    "Situação": sub_status,
-                    "Período": sub_period
-                }
+        O método é chamado quando ao receber os
+        dados da requisição a pagina do historico
+        acadêmico. Ao receber a página, ele varre a tabela
+        contendo as disciplinas, pegando os dados.
+        """
+        table = response.xpath('//table[@class="table table-bordered"]/tbody/tr')
+        for line in table:
+            data = line.xpath('./td//text()').getall()
+            sub_register = data[0]
+            sub_name = data[1]
+            sub_type = data[2]
+            sub_credits = data[3]
+            sub_workload = data[4]
+            sub_mean = data[5].strip().split('/n')[0]
+            sub_status = data[6]
+            sub_period = data[7]
+
+            yield  {
+                "Código": sub_register,
+                "Disciplina": sub_name,
+                "Tipo": sub_type,
+                "Créditos": sub_credits,
+                "Carga horária": sub_workload,
+                "Média": sub_mean,
+                "Situação": sub_status,
+                "Período": sub_period
+            }
